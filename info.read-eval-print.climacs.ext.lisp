5eff6c3f7cdd40925542fe86d8fff5f11e293e41
Yoshinori Tahara <read.eval.print@gmail.com>
2010-12-26 20:11:52 +0900

    New project info.read-eval-print.climacs.ext initialized by clbuild

---
+++ info.read-eval-print.climacs.ext.lisp
@@ -0,0 +1,6 @@
+;;; -*- mode: lisp; indent-tabs: nil -*-
+
+(in-package :info.read-eval-print.climacs.ext)
+
+(defun test ()
+  (format t "Hello World from new project info.read-eval-print.climacs.ext~%"))
---
+++ info.read-eval-print.climacs.ext.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+clbuild run any :info.read-eval-print.climacs.ext "(info.read-eval-print.climacs.ext:test)"
